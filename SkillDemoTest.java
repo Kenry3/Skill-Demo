6cd4fce831fd352ea69f0c4d34cb8535f1f96115
Kenry3 <88472170+Kenry3@users.noreply.github.com>
2022-02-04 12:24:29 -0800

    fix

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testAlwaysTrue() {
-        assertEquals(false, SkillDemo.alwaysTrue());// fails
+        assertEquals(true, SkillDemo.alwaysTrue());// fix
     }
 }
